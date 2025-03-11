c5ba69d6161b7b6fd96261e9bceba73f8bcaa10c
tracyn09 <tracyn0099@gmail.com>
2025-03-11 11:04:03 -0400

    Task 1–Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,7 @@
+//Task 1
+
+//Selecting riskDashboard Container
+const riskDashboard = document.getElementById(`riskDashboard`)
+console.log(`Risk Dashboard Loaded`);  // Printing to console
+
+//Task 2
