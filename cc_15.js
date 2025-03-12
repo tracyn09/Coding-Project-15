952cd67c35c20419e6b1718fb49cb5251fa5dee5
tracyn09 <tracyn0099@gmail.com>
2025-03-12 12:12:16 -0400

    Task 2–Adding Risk Items

--- cc_15.js
+++ cc_15.js
@@ -2,6 +2,58 @@
 
 //Selecting riskDashboard Container
 const riskDashboard = document.getElementById(`riskDashboard`)
-console.log(`Risk Dashboard Loaded`);  // Printing to console
+console.log(`Risk Dashboard Loaded`);                       // Printing to console
 
 //Task 2
+function addRiskItem(riskName, riskLevel, department) {
+
+    //Create new card
+    const riskCard = document.createElement (`div`)         
+    riskCard.classList.add(`riskCard`)
+
+    //Risk card style
+    riskCard.style.backgroundColor = "white"
+    riskCard.style.padding = "15px"
+    riskCard.style.margin = "10px 0"                        
+    riskCard.style.border = "1px solid lightgray"
+    riskCard.style.borderRadius = "8px"
+    riskCard.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.1)"
+
+    //Adding risk name, level, and department to Risk Card
+    const riskNameElement = document.createElement(`h3`)    
+    riskNameElement.textContent = riskName
+   
+    const riskLevelElement = document.createElement(`p`)    
+    riskLevelElement.textContent = `Risk Level: ${riskLevel}`
+
+    const departmentElement = document.createElement(`p`)  
+    departmentElement.textContent = `Department: ${department}`
+
+
+    //Appending to Dashboard
+    riskCard.appendChild(riskNameElement)
+    riskCard.appendChild(riskLevelElement)
+    riskCard.appendChild(departmentElement)
+    //new card to dashboard
+    document.getElementById(`riskDashboard`).appendChild(riskCard)
+}
+
+    //Inputting new risk to form
+    const riskForm = document.getElementById(`riskForm`)
+    riskForm.addEventListener('submit', function(event) {
+        event.preventDefault()
+
+    const riskName = document.getElementById(`riskName`).value
+    const riskLevel = document.getElementById(`riskLevel`).value
+    const department = document.getElementById(`department`).value
+
+    addRiskItem(riskName, riskLevel, department)
+
+    //Clear inputs for next entry
+    document.getElementById('riskName').value = ''
+    document.getElementById('riskLevel').value = 'Low'
+    document.getElementById('department').value = ''
+})
+//Test Case
+addRiskItem("Data Breach", "High", "IT")
+addRiskItem("Supply Chain Disruption", "Medium", "Operations")
